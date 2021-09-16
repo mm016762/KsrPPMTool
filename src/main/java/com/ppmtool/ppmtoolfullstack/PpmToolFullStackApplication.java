f800ac2cbec525288cfe06776778d2692437e1ec
Sudhakar katumalla <sudhakar.softpro@gmail.com>
2021-09-16 15:35:43 +0530

    first commit

---
+++ src/main/java/com/ppmtool/ppmtoolfullstack/PpmToolFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.ppmtool.ppmtoolfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolFullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmToolFullStackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ppmtool/ppmtoolfullstack/PpmToolFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppmtool.ppmtoolfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolFullStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
