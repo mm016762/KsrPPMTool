a3961a2e4a707c105dcff186244c024dd94f0190
Sudhakar katumalla <sudhakar.softpro@gmail.com>
2021-09-22 17:21:16 +0530

    Excection Handeling for project id

--- src/main/java/com/ppmtool/ppmtoolfullstack/services/ProjectService.java
+++ src/main/java/com/ppmtool/ppmtoolfullstack/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.ppmtool.ppmtoolfullstack.services;
 
 import com.ppmtool.ppmtoolfullstack.domain.Project;
+import com.ppmtool.ppmtoolfullstack.exceptions.ProjectIdException;
 import com.ppmtool.ppmtoolfullstack.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
+         try{
+             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+             return projectRepository.save(project);
+         }catch (Exception e){
+           throw new ProjectIdException("Project Id '"+project.getProjectIdentifier().toUpperCase()+"' already  exists");
+         }
 
-        return projectRepository.save(project);
     }
 }
