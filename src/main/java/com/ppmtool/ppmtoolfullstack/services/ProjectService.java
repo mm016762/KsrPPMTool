f3771ffd9ce4870a74331a5d69ddd775209020ea
Sudhakar katumalla <sudhakar.softpro@gmail.com>
2021-09-22 18:48:56 +0530

    All CURD Operation code added

--- src/main/java/com/ppmtool/ppmtoolfullstack/services/ProjectService.java
+++ src/main/java/com/ppmtool/ppmtoolfullstack/services/ProjectService.java
@@ -20,4 +20,24 @@ public class ProjectService {
          }
 
     }
+
+    public Project findProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project == null){
+            throw new ProjectIdException("Project Id '"+projectId+"' does not  exists");
+        }
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project == null){
+            throw new ProjectIdException("Can not delete project '"+projectId+"' It does not exist");
+        }
+        projectRepository.delete(project);
+    }
 }
